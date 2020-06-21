d1ea84dcf9c2be0e7d0f13d0e1cf68f46c8bedff
mehmetfatih2020 <67194269+mehmetfatih2020@users.noreply.github.com>
2020-06-21 06:52:30 +0300

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,6 +2,7 @@ public class TestClass {
 
     public static void main(String[] args) {
         System.out.println("Git'ten merhaba!");
+        System.out.println("GitHub'tan merhaba!");
     }
 }
 
